4162c27b7c099c7a2e4f00814c01aa5a80a75531
lilykoff <koffmanlily@gmail.com>
2026-03-06 16:11:45 -0500

    fix path typo

--- slurm/ft_capture24_sm.sh
+++ slurm/ft_capture24_sm.sh
@@ -35,9 +35,9 @@ cd "${REPO}"
 nvidia-smi
 
 # ---- EDIT THESE PATHS ----
-DATA_ROOT="/dcs05/ciprian/smart/ssl_wearables/capture24_100hz_w10_o0_small"   # must contain X.npy, Y.npy, pid.npy
-REPORT_ROOT="/dcs05/ciprian/smart/ssl_wearables/temp_reports/"                    # output CSVs
-WEIGHTS="/dcs05/ciprian/smart/ssl_wearables/weights/mtl_best.mdl"            # pretrained UKB model
+DATA_ROOT="/dcs05/ciprian/smart/ssl-wearables/capture24_100hz_w10_o0_small"   # must contain X.npy, Y.npy, pid.npy
+REPORT_ROOT="/dcs05/ciprian/smart/ssl-wearables/temp_reports/"                    # output CSVs
+WEIGHTS="/dcs05/ciprian/smart/ssl-wearables/weights/mtl_best.mdl"            # pretrained UKB model
 
 # Recommended: start modest, then scale up
 # - num_split=1 for speed
